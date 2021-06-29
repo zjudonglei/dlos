8fa2538b131d8ce4735a44673d7bed4c0d761354
donglei <zjudonglei@126.com>
2021-06-29 20:49:36 +0800

    第一天

---
+++ boot.asm
@@ -0,0 +1,20 @@
+	org 07c00h ; 将代码加载到这个地址
+;	org 0100h com文件
+; 小程序中 (<64K), 代码段和数据段放在一个段里
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+	call DispStr ; call == push eip + jmp
+	jmp $
+DispStr:
+	mov ax, BootMessage
+	mov bp, ax ; AH为13时，ES:BP表示串地址
+	mov cx, 16 ; AH为13时，表示串长度
+	mov ax, 01301h ; AH 中断入口
+	mov bx, 000ch ; BH 页号，AL为1时BL表示属性
+	mov dx, 0102h ; DH DL 起始行列
+	int 10h
+	ret
+BootMessage:	db "Hello, OS world!" ; 等价 BootMessage db "Hello, OS world!"，定义了一个字符串
+times 510 -($-$$) db 0 ; $-$$：本行距离程序开始处的相对距离
+dw 0xaa55 ;引导区结束标志，占2个字节
